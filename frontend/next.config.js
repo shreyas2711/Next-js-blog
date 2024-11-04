f0f5ba024dac267cbd96f8f95aedc46c4d248f81
Shreyas <shreyas.kamath27@gmail.com>
2024-11-04 23:30:46 +0530

    New commit

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,23 +1,29 @@
-// ./frontend/next.config.js
+    // ./frontend/next.config.js
+    
+    /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    domains: ['next-js-blog-vvss.onrender.com', 'images.pexels.com'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'next-js-blog-vvss.onrender.com',
-        pathname: '/uploads/**',
+    const Protocol = process.env.NEXT_PROTOCOL;
+    const Hostname = process.env.NEXT_HOSTNAME;
+    
+    const nextConfig = {
+      experimental: {
+        appDir: true,
       },
-      {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
+      images: {
+        domains: ['next-js-blog-vvss.onrender.com','images.pexels.com'], 
+        remotePatterns: [
+          {
+            protocol: `${Protocol}`,
+            hostname: `${Hostname}`,
+            port: '1337',
+            pathname: '/uploads/**',
+          },
+          {
+            protocol: `${Protocol}`,
+            hostname: 'images.pexels.com',
+          }
+        ],
       },
-    ],
-  },
-}
-
-module.exports = nextConfig;
+    }
+    
+    module.exports = nextConfig
