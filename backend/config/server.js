ddc6061bbc580db8d6ce701fba68c749784d5274
Shreyas <shreyas.kamath27@gmail.com>
2024-11-06 23:43:52 +0530

    New commit

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,8 @@
+const externalUrl = process.env.RENDER_EXTERNAL_URL;
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('RENDER_EXTERNAL_URL',`${externalUrl}`),
   app: {
     keys: env.array('APP_KEYS', ['myKeyA', 'myKeyB']),
   },
