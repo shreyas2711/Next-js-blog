9139b0653a11646e5d0bb46ad6b3f303fe638cdb
Shreyas <shreyas.kamath27@gmail.com>
2024-11-07 00:13:40 +0530

    nEW COMMIT

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,7 +1,24 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'img-src': ["'self'", 'data:', 'blob:', 'https://next-js-blog-vvss.onrender.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://next-js-blog-vvss.onrender.com'],
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://next-js-blog-vvss.onrender.com'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
